5671fd126ff12725f8c1311eefff4454018454ed
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2024-05-13 17:52:21 +0300

    assert

--- src/test/java/GetRequest.java
+++ src/test/java/GetRequest.java
@@ -1,12 +1,16 @@
 import io.restassured.RestAssured;
+import io.restassured.internal.common.assertion.Assertion;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class GetRequest {
     public static void main(String[] args) {
-        RestAssured.baseURI="https://reqres.in/api/users/1";
+        RestAssured.baseURI="https://reqres.in/api/users/2";
         RequestSpecification request = RestAssured.given();
         Response response = request.get();
         response.prettyPrint();
+
+        int statuscode = response.statusCode();
+        Assert.assertEquals(statuscode,201);
     }
 }
