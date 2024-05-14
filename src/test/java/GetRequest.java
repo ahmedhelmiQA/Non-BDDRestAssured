14d3147b5d5515a5f3a5de96d0f9e80d0cfb522e
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2024-05-14 15:59:52 +0300

    add testNG

--- src/test/java/GetRequest.java
+++ src/test/java/GetRequest.java
@@ -2,10 +2,11 @@ import io.restassured.RestAssured;
 import io.restassured.internal.common.assertion.Assertion;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 
 public class GetRequest {
     public static void main(String[] args) {
-        RestAssured.baseURI="https://reqres.in/api/users/2";
+        RestAssured.baseURI="https://reqres.in/api/unknown";
         RequestSpecification request = RestAssured.given();
         Response response = request.get();
         response.prettyPrint();
